2187adc37d09ded3239d0ed57e4ac5a7e1672384
Ramesh Ramaswamy <ramesh1441@gmail.com>
2022-11-27 15:51:57 -0800

    added java functions

--- assets/script.js
+++ assets/script.js
@@ -20,45 +20,8 @@ function selectTopic() {
    console.log('Please try again!');
  }
 }
-<<<<<<< HEAD
-*/
-/*
-var topics = ['HTML','CSS','GIT','JavaScript']
-for(var x = 0; x < topics.length; x++){
-    console.log(topics[x]);
-}
-*/
-
-var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
-var randomTopic = topics[Math.floor(Math.random() * topics.length)];
-
-function listTopics() {
- for (var x = 0; x < topics.length; x++) {
-   console.log(topics[x]);
- }
-}
-
-function selectTopic() {
- if (randomTopic === 'HTML') {
-   console.log("Let's study HTML!");
- } else if (randomTopic === 'CSS') {
-   console.log("Let's study CSS!");
- } else if (randomTopic === 'Git') {
-   console.log("Let's study Git!");
- } else if (randomTopic === 'JavaScript') {
-   console.log("Let's study JavaScript!");
- } else {
-   console.log('Please try again!');
- }
-}
-=======
->>>>>>> 48f8c522faebb0d6d6935c710eb06f973f33fc40
 
 console.log('Here are the topics we learned through Prework:');
 listTopics();
 console.log('Which topic should we study first?');
-<<<<<<< HEAD
 selectTopic();
-=======
-selectTopic();
->>>>>>> 48f8c522faebb0d6d6935c710eb06f973f33fc40
